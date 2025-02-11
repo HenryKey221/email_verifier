21a4f6d0f4e5d3f3abf1fd331c6ea01e6839dcdd
henry <henrykey221@gmail.com>
2025-02-11 22:28:26 +0900

    fix init

--- email_verifier/email_verifier/settings.py
+++ email_verifier/email_verifier/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = "email_verifier.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verifier',
+        'USER': 'Henry',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',  # Use '127.0.0.1' if you're connecting locally
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
